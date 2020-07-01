03fde789226d183704becba6d1df1745731174b0
demonic <ian910297@gmail.com>
2020-07-01 20:24:06 +0800

    Add complete format-checker test

---
+++ scripts/get_comic_home.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# please add the target comic_name at the start of this code, example:
+
+import requests
+import urllib
+import json
+import sys
+
+from bs4 import BeautifulSoup as Soup
+
+def doRequest(url):
+    return requests.get(url, cookies={'RI': '0'})
+
+def searchComic(pattern):
+    resp = doRequest(
+            'https://comicbus.com/member/search.aspx?' + \
+            urllib.parse.urlencode({"k": pattern}, encoding='big5'))
+
+    page = Soup(resp.content.decode('big5'), features="html.parser")
+    rows = page.find_all('td', style="border-bottom:1px dotted #cccccc; line-height:18px; padding-left:5px ")
+
+    results = []
+    for row in rows:
+        results.extend(row.find_all('a', href=True))
+
+    ret = {}
+    for r in results:
+        title = r.find('font').getText()
+        if title == pattern:
+            return 'https://comicbus.com' + r['href']
+
+    return None
+
+if __name__== '__main__':
+    if len(sys.argv) > 1:
+        comic_name = sys.argv[1]
+    else:
+        comic_name = '食戟之靈'
+    comic_url = searchComic(comic_name)
+
+    print(json.dumps(comic_url), end = '')
+
--- worker/format-checker.py
+++ worker/format-checker.py
@@ -8,25 +8,21 @@ class FormatChecker:
         self.img_checker = re.compile(r'https://img4.8comic.com/\d+/\d+/\d+/(\d+)_\w+.jpg')
 
     def is_home_url(self, data):
-        print("is home url?")
         result = self.home_checker.match(data)
         if result:
-            print("match")
+            return True
         else:
-            print("no")
+            return False
 
     def is_episode_url(self, data):
-        print("is episode url?")
         for url in data:
             result = self.episode_checker.match(url)
             if result:
-                print("match")
+                return True
             else:
-                print("no")
+                return False
 
     def is_ordered_page_number(self, data):
-        print("is ordered page number?")
-        
         current_length = 0
         total_length = len(data)
         start = None
@@ -40,51 +36,76 @@ class FormatChecker:
                     start = int(result.group(1))
                     current_length += 1
                 else:
-                    print("no")
-                    return
+                    return False
             else:
-                print("no")
-                return
+                return False
         if current_length != total_length:
-            print("no")
+            return False
         else:
-            print("match")
+            return True
         
 
 if __name__ == "__main__":
     checker = FormatChecker()
-    home_urls = [
-        "https://comicbus.com/html/9337.html",
-        "https://comicbus.com/html/933",
-        "https://comicbus.com/html/.html",
-        "https://comicbus.com/html/9.hml"
-    ]
-    for url in home_urls:
-        checker.is_home_url(url)
+    import json
+    from subprocess import Popen, PIPE
 
-    episode_urls = [
-        "https://comicbus.live/online/a-9337.html?ch=311",
-        "https://comicbus.live/online/a-9337.html?ch=312",
-        "https://comicbus.live/online/a-9337.html?ch=31"
-    ]
-    checker.is_episode_url(episode_urls)
+    execute_command = ["../scripts/get_comic_home.py"]
+    p = Popen(execute_command, stdout=PIPE, stderr=PIPE)
+    out, err = p.communicate()
+    home_url = json.loads(out)
+    ## static test data
+    # home_urls = [
+    #     "https://comicbus.com/html/9337.html",
+    #     "https://comicbus.com/html/933",
+    #     "https://comicbus.com/html/.html",
+    #     "https://comicbus.com/html/9.hml"
+    # ]
+    if checker.is_home_url(home_url):
+        print("correct home_url")
+    else:
+        print("incorrect home_url")
 
-    img_urls = [
-        "https://img4.8comic.com/4/17838/0/001_S98.jpg",
-        "https://img4.8comic.com/4/17838/0/002_bUU.jpg",
-        "https://img4.8comic.com/4/17838/0/003_4vM.jpg",
-        "https://img4.8comic.com/4/17838/0/004_HHH.jpg",
-        "https://img4.8comic.com/4/17838/0/005_d7c.jpg",
-        "https://img4.8comic.com/4/17838/0/006_Gh8.jpg"
-    ]
-    checker.is_ordered_page_number(img_urls)
+    
+    execute_command = ["../scripts/get_episode_urls.py"]
+    p = Popen(execute_command, stdout=PIPE, stderr=PIPE)
+    out, err = p.communicate()
+    episode_urls = json.loads(out)
+    ## static test data
+    # episode_urls = [
+    #     "https://comicbus.live/online/a-9337.html?ch=311",
+    #     "https://comicbus.live/online/a-9337.html?ch=312",
+    #     "https://comicbus.live/online/a-9337.html?ch=31"
+    # ]
+    print(episode_urls)
+    if checker.is_episode_url(episode_urls):
+        print("correct episode_url")
+    else:
+        print("incorrect episode_url")
 
-    img_urls = [
-        "https://img4.8comic.com/4/17838/0/001_S98.jpg",
-        "https://img4.8comic.com/4/17838/0/002_bUU.jpg",
-        "https://img4.8comic.com/4/17838/0/003_4vM.jpg",
-        "https://img4.8comic.com/4/17838/0/004_HHH.jpg",
-        "https://img4.8comic.com/4/17838/0/005_d7c.jpg",
-        "https://img4.8comic.com/4/17838/0/007_Gh8.jpg"
-    ]
-    checker.is_ordered_page_number(img_urls)
+    execute_command = ["../scripts/get_images.py"]
+    p = Popen(execute_command, stdout=PIPE, stderr=PIPE)
+    out, err = p.communicate()
+    img_urls = json.loads(out)
+    ## static test data
+    # img_urls = [
+    #     "https://img4.8comic.com/4/17838/0/001_S98.jpg",
+    #     "https://img4.8comic.com/4/17838/0/002_bUU.jpg",
+    #     "https://img4.8comic.com/4/17838/0/003_4vM.jpg",
+    #     "https://img4.8comic.com/4/17838/0/004_HHH.jpg",
+    #     "https://img4.8comic.com/4/17838/0/005_d7c.jpg",
+    #     "https://img4.8comic.com/4/17838/0/006_Gh8.jpg"
+    # ]
+    ## wrong page number
+    # img_urls = [
+    #     "https://img4.8comic.com/4/17838/0/001_S98.jpg",
+    #     "https://img4.8comic.com/4/17838/0/002_bUU.jpg",
+    #     "https://img4.8comic.com/4/17838/0/003_4vM.jpg",
+    #     "https://img4.8comic.com/4/17838/0/004_HHH.jpg",
+    #     "https://img4.8comic.com/4/17838/0/005_d7c.jpg",
+    #     "https://img4.8comic.com/4/17838/0/007_Gh8.jpg"
+    # ]
+    if checker.is_ordered_page_number(img_urls):
+        print("correct page number")
+    else:
+        print("incorrect page number")
